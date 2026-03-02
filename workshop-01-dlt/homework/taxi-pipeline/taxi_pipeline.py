c9617b4f689857a5627cc3e2607c8a34cee4d629
Vijak-k <vijak_k@hotmail.com>
2026-03-02 23:56:36 +0700

    update homework workshop dlt

---
+++ workshop-01-dlt/homework/taxi-pipeline/taxi_pipeline.py
@@ -0,0 +1,49 @@
+"""NYC Taxi Data REST API source for dlt."""
+
+import dlt
+from dlt.sources.rest_api import rest_api_resources
+from dlt.sources.rest_api.typing import RESTAPIConfig
+
+
+@dlt.source
+def taxi_pipeline_rest_api_source():
+    """Define dlt resources from NYC taxi data REST API endpoints."""
+    config: RESTAPIConfig = {
+        "client": {
+            "base_url": "https://us-central1-dlthub-analytics.cloudfunctions.net/",
+        },
+        "resources": [
+            {
+                "name": "taxi_data",
+                "endpoint": {
+                    "path": "data_engineering_zoomcamp_api",
+                    "paginator": {
+                        "type": "page_number",
+                        "base_page": 1,
+                        "page_param": "page",
+                        "total_path": None,
+                    },
+                },
+            },
+        ],
+    }
+
+    yield from rest_api_resources(config)
+
+
+pipeline = dlt.pipeline(
+    pipeline_name='taxi_pipeline_pipeline',
+    destination='duckdb',
+    dataset_name="taxi_data",
+    # `refresh="drop_sources"` ensures the data and the state is cleaned
+    # on each `pipeline.run()`; remove the argument once you have a
+    # working pipeline.
+    refresh="drop_sources",
+    # show basic progress of resources extracted, normalized files and load-jobs on stdout
+    progress="log",
+)
+
+
+if __name__ == "__main__":
+    load_info = pipeline.run(taxi_pipeline_rest_api_source())
+    print(load_info)  # noqa: T201
